ad5086958ecf22fdc701dc9c98d50faa9f53450d
Marco Moschettini <m.marmos@gmail.com>
2015-12-14 20:24:30 +0100

    Added all needed exceptions and tests.

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ function __total_buffers_size(object)
   if(object instanceof Buffer)
     return object.length
 
+  if(typeof object === "string")
+    return 0;
+
   var size = 0;
 
   for(var i in object)
@@ -29,6 +32,9 @@ function __extract_buffers(binary, object)
     return meta;
   }
 
+  if(typeof object === "string")
+    return object;
+
   for(var i in object)
     object[i] = __extract_buffers(binary, object[i]);
 
@@ -39,11 +45,20 @@ function __insert_buffers(binary, object)
 {
   if(object && object.type === "__bufferify_placeholder__")
   {
+    if(!Number.isInteger(object.offset) || !Number.isInteger(object.length))
+      throw {'code': 4, 'description': 'Placeholder corrupted', 'url': ''};
+
+    if(binary.length < object.offset + object.length)
+      throw {'code': 5, 'description': 'Placeholder references data beyond end of buffer', 'url': ''};
+
     var buffer = new Buffer(object.length);
     binary.copy(buffer, 0, object.offset, object.offset + object.length);
 
     return buffer;
   }
+
+  if(typeof object === "string")
+    return object;
 
   for(var i in object)
     object[i] = __insert_buffers(binary, object[i]);
@@ -71,11 +86,32 @@ function pack(object)
 
 function unpack(buffer)
 {
-  buffer = zlib.gunzipSync(buffer);
+  try
+  {
+    buffer = zlib.gunzipSync(buffer);
+  }
+  catch (error)
+  {
+    throw {'code': 0, 'description': 'Gunzip failed', 'url': ''};
+  }
+
+  if(buffer.length < 4)
+    throw {'code': 1, 'description': 'Buffer should be at least 4 bytes long', 'url': ''};
 
   // Add some exception..?
   var meta_length = buffer.readUInt32LE();
-  var meta = jsonpack.unpack(buffer.toString("utf8", 4, 4 + meta_length));
+
+  if(buffer.length < meta_length + 4)
+    throw {'code': 2, 'description': 'Meta data is shorter than declared', 'url': ''};
+
+  try
+  {
+    var meta = jsonpack.unpack(buffer.toString("utf8", 4, 4 + meta_length));
+  }
+  catch (error)
+  {
+    throw {'code': 3, 'description': 'Meta is corrupted', 'url': ''};
+  }
 
   var binary = new Buffer(buffer.length - meta_length - 4);
   buffer.copy(binary, 0, 4 + meta_length, buffer.length);
--- test/index.js
+++ test/index.js
@@ -1,18 +1,107 @@
 'use strict';
 
 var assert = require('assert');
+var zlib = require('zlib');
 var bufferify = require('../main');
 
 describe('bufferify', function ()
 {
   it('should pack and unpack json', function()
   {
-
-    var original = {1: new Buffer(1024), "a": 3, "b": [1, 2, 3, new Buffer(2), new Buffer(2)]};
+    var original = {1: new Buffer(1024), "a": 3, "b": [1, 2, 3, new Buffer(2), new Buffer(2), "test string"]};
 
     var packed = bufferify.pack(original);
     var unpacked = bufferify.unpack(packed);
 
     assert.deepEqual(original, unpacked, 'Buffers not equal');
+  });
+
+  it('should throw error 0 when gzip fails', function()
+  {
+    try
+    {
+      bufferify.unpack(new Buffer(1024));
+      assert(false);
+    }
+    catch (error)
+    {
+      assert.equal(error.code, 0);
+    }
+  });
+
+  it('should throw error 1 when buffer is smaller than 4 bytes', function()
+  {
+    try
+    {
+      bufferify.unpack(zlib.gzipSync(new Buffer(3)));
+      assert(false);
+    }
+    catch (error)
+    {
+      assert.equal(error.code, 1);
+    }
+  });
+
+  it('should throw error 2 when meta is smaller than declared', function()
+  {
+    try
+    {
+      var buffer = new Buffer(4);
+      buffer.writeUInt32LE(1024);
+
+      bufferify.unpack(zlib.gzipSync(buffer));
+      assert(false);
+    }
+    catch (error)
+    {
+      assert.equal(error.code, 2);
+    }
+  });
+
+  it('should throw error 3 when meta is corrupted', function()
+  {
+    try
+    {
+      var buffer = new Buffer(14);
+      buffer.writeUInt32LE(10);
+      buffer.write("Wrong data!", 10);
+
+      bufferify.unpack(zlib.gzipSync(buffer));
+      assert(false);
+    }
+    catch (error)
+    {
+      assert.equal(error.code, 3);
+    }
+  });
+
+  it('should throw error 4 when placeholder is corrupted', function()
+  {
+    var buffer = bufferify.pack({"type": "__bufferify_placeholder__"});
+
+    try
+    {
+      bufferify.unpack(buffer);
+      assert(false);
+    }
+    catch (error)
+    {
+      assert.equal(error.code, 4);
+    }
+  });
+
+  it('should throw error 5 when placeholder references data beyond the end of buffer', function()
+  {
+    var buffer = bufferify.pack({"type": "__bufferify_placeholder__", "offset": 0, "length": 1});
+
+    try
+    {
+      bufferify.unpack(buffer);
+      assert(false);
+    }
+    catch (error)
+    {
+      assert.equal(error.code, 5);
+    }
   });
 });
