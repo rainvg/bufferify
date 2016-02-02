3b38137dce22ea5b8b3a51929854d36f31911326
Matteo Monti <monti@MacBook-Pro-di-Matteo.local>
2016-02-02 15:02:57 +0100

    Edited authors, fixed settings, fixed tests.

--- test/index.js
+++ test/index.js
@@ -2,13 +2,13 @@
 
 var assert = require('assert');
 var zlib = require('zlib');
-var bufferify = require('../dist/index');
+var bufferify = require('../src/index.js');
 
 describe('bufferify', function ()
 {
   it('should pack and unpack json', function()
   {
-    var original = {1: new Buffer(1024), "a": 3, "b": [1, 2, 3, new Buffer(2), new Buffer(2), "test string"]};
+    var original = {1: new Buffer(1024), 'a': 3, 'b': [1, 2, 3, new Buffer(2), new Buffer(2), 'test string']};
 
     var packed = bufferify.pack(original);
     var unpacked = bufferify.unpack(packed);
@@ -64,7 +64,7 @@ describe('bufferify', function ()
     {
       var buffer = new Buffer(14);
       buffer.writeUInt32LE(10);
-      buffer.write("Wrong data!", 10);
+      buffer.write('Wrong data!', 10);
 
       bufferify.unpack(zlib.gzipSync(buffer));
       assert(false);
@@ -77,7 +77,7 @@ describe('bufferify', function ()
 
   it('should throw error 4 when placeholder is corrupted', function()
   {
-    var buffer = bufferify.pack({"type": "__bufferify_placeholder__"});
+    var buffer = bufferify.pack({'type': '__bufferify_placeholder__'});
 
     try
     {
@@ -92,7 +92,7 @@ describe('bufferify', function ()
 
   it('should throw error 5 when placeholder references data beyond the end of buffer', function()
   {
-    var buffer = bufferify.pack({"type": "__bufferify_placeholder__", "offset": 0, "length": 1});
+    var buffer = bufferify.pack({'type': '__bufferify_placeholder__', 'offset': 0, 'length': 1});
 
     try
     {
