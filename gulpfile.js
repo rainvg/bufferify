58030b1413f26f8387f4a39412692dd3c76ab92e
Marco Moschettini <m.marmos@gmail.com>
2016-05-10 11:35:21 +0200

    Gulp file updated.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp  = require('gulp'),
     mocha = require('gulp-mocha'),
     rename = require('gulp-rename');
 
-gulp.task('default', ['lint','test', 'minify'], function()
+gulp.task('default', ['lint', 'test', 'minify'], function()
 {
 });
 
@@ -15,15 +15,15 @@ gulp.task('lint', function()
   return gulp.src('./src/*.js').pipe(jshint()).pipe(jshint.reporter('jshint-stylish')).pipe(jshint.reporter('fail'));
 });
 
-gulp.task('minify', ['lint', 'test'], function()
-{
-  return gulp.src('src/index.js').pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist'));
-});
-
 gulp.task('test', ['lint'], function()
 {
   return gulp.src('test/index.js', {read: false}).pipe(mocha()).once('error', function()
   {
     process.exit(1);
   });
+});
+
+gulp.task('minify', ['lint', 'test'], function()
+{
+  return gulp.src('src/index.js').pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist'));
 });
